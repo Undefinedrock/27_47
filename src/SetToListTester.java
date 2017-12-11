8d5a540e09294e3ec55d10742147c03283ff77d0
ushallnotpas <zhanghanfei1991@hotmail.com>
2017-12-11 14:35:24 -0500

    Initial commit

---
+++ src/SetToListTester.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public static<E> ArrayList<E> setToList(Set<E>s){
+
+
+        public class SetToListTester{
+         public static void main(String[]args){
+
+
+             Set<Double> mySet= new HashSet<>();
+
+             for (int looper=0; looper<20; looper++){
+
+
+                 mySet.add(Math.random()*100);
+             }
+             System.out.println("Status of set is"+ mySet);
+             System.out.println();
+             ArrayList<Double>myArray= new ArrayList<>();
+             myArray= setToList(mySet);
+             System.out.println("Status of Array is"+myArray);
+             System.out.println();
+         }
+public static<E> ArrayList<E>setToList(Set<E>s){
+             ArrayList<E>newlist= new ArrayList<>();
+}
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
